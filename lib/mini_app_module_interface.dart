7dfccabd1c9dea67f8c73b0b1b75df2a725dff9f
thelonewolfman <thelonewolf.init@gmail.com>
2025-08-19 11:45:38 +0700

    update

--- lib/mini_app_module_interface.dart
+++ lib/mini_app_module_interface.dart
@@ -8,6 +8,12 @@ export 'src/core/module_config.dart';
 export 'src/core/host_communication.dart';
 export 'src/core/module_lifecycle.dart';
 export 'src/core/module_base.dart';
+export 'src/core/navigation_handler.dart';
+export 'src/core/navigation_abstractions.dart';
+export 'src/core/clean_mini_app_base.dart';
+// export 'src/core/clean_shortcut_interfaces.dart';
+export 'src/core/clean_shortcut_widgets.dart';
+export 'src/core/initializable_page_interface.dart';
 
 // Models exports
 export 'src/models/module_user.dart';
